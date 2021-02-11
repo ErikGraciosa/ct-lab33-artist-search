7e23bc7fa480a72ed2e9dde05b543bedd1688fe8
ErikGraciosa <erikgraciosa@gmail.com>
2021-02-11 13:32:42 -0800

    pagination added

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -24,8 +24,10 @@ export default function Home() {
       <Search setSearch={setSearch} />
       {(offset) != 0 
         ? <button onClick={() => setOffset(offset - 25)}>Last Page</button> 
-        : null}
-      <button onClick={() => setOffset(offset + 25)}>Next Page</button>
+        : <button disabled>Last Page</button>}
+      {(search) === '' 
+        ? <button disabled>Next Page</button>
+        : <button onClick={() => setOffset(offset + 25)}>Next Page</button>}
       {loading ? <div>Loading</div> : <ArtistDisplay artists={artists} />}
     </div>
   );
