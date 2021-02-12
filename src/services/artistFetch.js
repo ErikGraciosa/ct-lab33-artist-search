ccbcd5229b8e08188c90ff3d03fc4171d1b9f15e
ErikGraciosa <erikgraciosa@gmail.com>
2021-02-12 15:55:41 -0800

    fix https

--- src/services/artistFetch.js
+++ src/services/artistFetch.js
@@ -1,6 +1,6 @@
 export const fetchArtists = (artist, offset) => {
   return fetch(
-    `http://musicbrainz.org/ws/2/artist?query=${artist}&fmt=json&limit=25&offset=${offset}` // eslint-disable-line
+    `https://musicbrainz.org/ws/2/artist?query=${artist}&fmt=json&limit=25&offset=${offset}` // eslint-disable-line
   )
     .then((res) => res.json())
     .then((json) => json.artists);
--- src/services/fetchAlbum.js
+++ src/services/fetchAlbum.js
@@ -1,5 +1,5 @@
 export const fetchAlbum = (artistId) => {
-  return fetch(`http://musicbrainz.org/ws/2/release?artist=${artistId}&fmt=json`) // eslint-disable-line
+  return fetch(`https://musicbrainz.org/ws/2/release?artist=${artistId}&fmt=json`) // eslint-disable-line
     .then((res) => res.json())
     .then((json) => json.releases);
 };
